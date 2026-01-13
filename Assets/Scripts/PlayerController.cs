f3ae2ef15a604830c1b2022593dcba2d1407a2e9
tonipek <tonipek@metropolia.fi>
2026-01-13 14:33:16 +0200

    chapter 5
    player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed;
+    Rigidbody2D _rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _rb = gameObject.GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandlePlayerMovement();
+    }
+
+    private void HandlePlayerMovement()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        movement = Vector2.ClampMagnitude(movement, 1.0f);
+        _rb.linearVelocity = movement * movementSpeed;
+    }
+        
+}
